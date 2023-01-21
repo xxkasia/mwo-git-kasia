17d482b3747b270d4f5bdef94ad6d6b7a49e16fc
Patrycja Szczepanska <psz@student.agh.edu.pl>
2023-01-21 11:42:25 +0100

    New method created

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,4 +3,7 @@ public class SomeProgram {
       System.out.println("We are learning to use Git."); 
       System.out.println("We will be Git experts, soon.");
     }
+    public void startTheRocket(int fuel) {
+    // TODO implement
+}
 }
